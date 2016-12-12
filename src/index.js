20c13553478fed423e7f8a30c75d5b285c48a19d
Badr AZIZBI <azizbibadr@gmail.com>
2016-12-12 14:54:33 +0100

    Fixing errors

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-import { Buffer } from 'buffer'
-import net from 'net'
 import crc32 from 'buffer-crc32'
+import net from 'net'
+import { Buffer } from 'buffer'
 
 // ===================================================================
 
@@ -14,7 +14,7 @@ export const configurationSchema = {
     },
     port: {
       type: 'integer',
-      description: 'The snca port'
+      description: 'The NSCA port'
     },
     key: {
       type: 'string',
@@ -32,22 +32,44 @@ export const configurationSchema = {
   additionalProperties: false,
   required: ['server', 'port', 'key', 'host', 'service']
 }
-export const testSchema = {
-  type: 'object',
 
-  properties: {
-    status: {
-      type: 'integer',
-      description: 'The service status in Nagios (0: OK | 1: WARNING | 2: CRITICAL)'
+// ===================================================================
+
+function nscaPacketBuilder (host, service, size, version, timestamp, iv, status, message, encoding) {
+  // Building NSCA packet
+  let packet = new Buffer(size)
+  packet.fill(0)
+  packet.writeInt16BE(version, 0)
+  packet.fill('h', 2, 3)
+  packet.writeUInt32BE(0, 4) // initial 0 for CRC32 value
+  packet.writeUInt32BE(timestamp, 8)
+  packet.writeInt16BE(status, 12)
+  packet.write(host, 14, 77, encoding)
+  packet.write(service, 78, 206, encoding)
+  packet.write(message, 206, size, encoding)
+  packet.writeUInt32BE(crc32.unsigned(packet), 4)
+  packet = Buffer.from(packet.toString(encoding), 'ascii')
+  return packet
+}
+
+function xor (long, short) {
+  const result = []
+  let j = 0
+  for (let i = 0; i < long.length; i++) {
+    if (j === short.length) {
+      j = 0
     }
-  },
-  additionalProperties: false,
-  required: ['status']
+    result[i] = long[i] ^ short[j]
+    j++
+  }
+  return result
 }
 
 // ===================================================================
+
 const SIZE = 720
 const VERSION = 3
+
 class XoServerNagios {
 
   constructor ({ xo }) {
@@ -71,65 +93,53 @@ class XoServerNagios {
     this._unset()
   }
 
-  test ({status}) {
-    return this._sendPassiveCheck(status, 'The server-nagios plugin for Xen Orchestra server seems to be working fine, nicely done :)')
+  test () {
+    return this._sendPassiveCheck({
+      status: 0,
+      message: 'The server-nagios plugin for Xen Orchestra server seems to be working fine, nicely done :)'
+    })
   }
 
-  _sendPassiveCheck (status, message) {
+  _sendPassiveCheck ({
+    status,
+    message
+  }) {
     const client = new net.Socket()
-    client.connect(this._conf.port, this._conf.server, () => {
-      console.log('Successful connection')
-    })
-    client.on('data', data => {
-      const encoding = 'binary'
-      const dataBuffer = new Buffer(data)
-      const iv = dataBuffer.toString(encoding, 0, 128) // initialization vector
-      const timestamp = dataBuffer.readInt32BE(128)
-      const paquet = this._nscaPaquetBuilder(SIZE, VERSION, timestamp, iv, status, message, encoding)
-      // 1) Using xor between the NSCA paquet and the initialization vector
-      // 2) Using xor between the result of the first operation and the encryption key
-      const xorPaquetBuffer = new Buffer(this._xor(this._xor(this._stringToAsciiArray(paquet.toString(encoding)), this._stringToAsciiArray(iv)), this._stringToAsciiArray(this._conf.key)), encoding)
-      client.write(xorPaquetBuffer, function (a) {
-        client.destroy()
+
+    return new Promise((resolve, reject) => {
+      client.connect(this._conf.port, this._conf.server, () => {
+        console.log('Successful connection')
+      })
+
+      client.on('data', data => {
+        const encoding = 'binary'
+        const timestamp = data.readInt32BE(128)
+        const packet = nscaPacketBuilder(this._conf.host, this._conf.service, SIZE, VERSION, timestamp, iv, status, message.toString().replace(/\s/g, ' '), encoding)
+        const iv = Buffer.from(data.toString(encoding, 0, 128), 'ascii') // initialization vector
+        const key = Buffer.from(this._conf.key, 'ascii')
+
+        // 1) Using xor between the NSCA packet and the initialization vector
+        // 2) Using xor between the result of the first operation and the encryption key
+        const xorPacketBuffer = new Buffer(
+          xor(
+            xor(
+              packet,
+              iv
+            ),
+            key
+          ),
+          encoding
+        )
+        client.write(xorPacketBuffer, (a) => {
+          client.destroy()
+          resolve(a)
+        })
+      })
+
+      client.on('error', (err) => {
+        reject(err)
       })
     })
-  }
-
-  _nscaPaquetBuilder (size, version, timestamp, iv, status, message, encoding) {
-    // Building nsca paquet
-    const paquet = new Buffer(size)
-    paquet.fill(0)
-    paquet.writeInt16BE(version, 0)
-    paquet.fill('h', 2, 3)
-    paquet.writeUInt32BE(0, 4) // initial 0 for CRC32 value
-    paquet.writeUInt32BE(timestamp, 8)
-    paquet.writeInt16BE(status, 12)
-    paquet.write(this._conf.host, 14, 77, encoding)
-    paquet.write(this._conf.service, 78, 206, encoding)
-    paquet.write(message, 206, size, encoding)
-    paquet.writeUInt32BE(crc32.unsigned(paquet), 4)
-    return paquet
-  }
-
-  _stringToAsciiArray (str) {
-    const map = Array.prototype.map
-    const chars = map.call(str, x => {
-      return x.charCodeAt(0)
-    })
-    return chars
-  }
-
-  _xor (long, short) {
-    const result = []
-    let j = 0
-    for (let i = 0; i < long.length; i++) {
-      if (j === short.length) {
-        j = 0
-      }
-      result[i] = long[i] ^ short[j]
-      j++
-    }
-    return result
   }
 }
 
